4e1fc4e1e598e1cf224c47400fd2daebb49fb16a
MatiasManevi <manevimatias@gmail.com>
2026-02-20 11:33:16 -0300

    Add login experimental route

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
-from app.routers import documents
+from app.routers import documents, auth
 
 app = FastAPI()
 
 app.include_router(documents.router)
+app.include_router(auth.router)
--- backend/app/routers/documents.py
+++ backend/app/routers/documents.py
@@ -5,6 +5,7 @@ from app.schemas.document import DocumentResponse
 from app.services.storage.factory import get_storage_service
 from app.services.storage.service import StorageService
 from app.services.document import DocumentService
+from app.core.auth import get_current_user
 
 router = APIRouter(prefix="/documents", tags=["Documents"])
 
@@ -17,8 +18,8 @@ router = APIRouter(prefix="/documents", tags=["Documents"])
 async def upload_document(
     file: UploadFile = File(...),
     # db: Session = Depends(get_db),
+    current_user=Depends(get_current_user),
     storage_service: StorageService = Depends(get_storage_service),
 ):
-    current_user_id = 'abc123'
     service = DocumentService(storage_service)
-    return await service.upload_document(file, current_user_id)
+    return await service.upload_document(file, current_user.id)
